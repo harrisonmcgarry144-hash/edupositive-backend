1f39501c627b9433300f0dc8ff77788bbbf3a66f
harrisonmcgarry144-hash <harrisonmcgarry144@gmail.com>
2026-04-25 01:05:23 +0100

    Add full auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+const authService = require("../services/auth.service");
+
+exports.register = async (req, res, next) => {
+  try {
+    const data = await authService.register(req.body);
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const data = await authService.login(req.body);
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.refresh = async (req, res, next) => {
+  try {
+    const data = await authService.refresh(req.body.token);
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+};
